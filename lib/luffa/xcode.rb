cc7953a1aebc0de00fee62d599928012df0fb6c5
Joshua Moody <joshuajmoody@gmail.com>
2015-03-22 17:39:18 +0100

    Luffa::Xcode can find info about active Xcode

--- lib/luffa/xcode.rb
+++ lib/luffa/xcode.rb
@@ -13,5 +13,26 @@ module Luffa
         ENV['DEVELOPER_DIR'] = original_developer_dir
       end
     end
+
+    # The developer dir at the time the tests start.
+    #
+    # To change the active Xcode:
+    #
+    #   $ DEVELOPER_DIR=/path/to/Xcode.app/Contents/Developer be rake spec
+    def active_xcode_dir
+      @active_xcode_dir ||= lambda {
+        env = Luffa::Environment.developer_dir
+        if env
+          env
+        else
+          # fall back to xcode-select
+          xcode_select_path
+        end
+      }.call
+    end
+
+    def xcode_select_path
+      @xcode_select_dir ||= `xcode-select --print-path`.chomp
+    end
   end
 end
--- spec/lib/xcode_spec.rb
+++ spec/lib/xcode_spec.rb
@@ -1,8 +1,9 @@
 describe Luffa::Xcode do
 
+  let(:dev_dir) { '/Xcode/4.6.3/Xcode.app/Contents/Developer'}
+
   describe '#with_developer_dir' do
     it 'respects the original value of DEVELOPER_DIR' do
-      dev_dir = '/Xcode/4.6.3/Xcode.app/Contents/Developer'
       stub_env('DEVELOPER_DIR', dev_dir)
       begin
         Luffa::Xcode.instance.with_developer_dir('/Some/Other/Dir') do
@@ -12,6 +13,35 @@ describe Luffa::Xcode do
 
       end
       expect(ENV['DEVELOPER_DIR']).to be == dev_dir
+    end
+  end
+
+  it '#xcode_select_path' do
+    expect(Luffa::Xcode.instance.xcode_select_path).not_to be == nil
+  end
+
+  describe '#active_xcode_dir' do
+    let(:xcode) { Luffa::Xcode.instance }
+
+    before(:each) {
+      xcode.instance_variable_set(:@active_xcode_dir, nil)
+      xcode.instance_variable_set(:@xcode_select_dir, nil)
+    }
+
+    after(:each) {
+      xcode.instance_variable_set(:@active_xcode_dir, nil)
+      xcode.instance_variable_set(:@xcode_select_dir, nil)
+    }
+
+    it 'respects the DEVELOPER_DIR' do
+      stub_env('DEVELOPER_DIR', dev_dir)
+      expect(xcode.active_xcode_dir).to be == dev_dir
+    end
+
+    it 'returns the xcode-select --print-path' do
+      stub_env({'DEVELOPER_DIR' => nil})
+      expect(xcode).to receive(:xcode_select_path).and_return(dev_dir)
+      expect(xcode.active_xcode_dir).to be == dev_dir
     end
   end
 end
